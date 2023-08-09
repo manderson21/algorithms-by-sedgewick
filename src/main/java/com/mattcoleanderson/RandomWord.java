186c14b2de089300f6d602df124334a4f5eb98d5
matthewanderson <manderson1@firstorion.com>
2023-08-09 16:56:04 -0500

    Made progress on the random word picker

--- src/main/java/com/mattcoleanderson/RandomWord.java
+++ src/main/java/com/mattcoleanderson/RandomWord.java
@@ -1,7 +1,22 @@
 package com.mattcoleanderson;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
   public static void main(String[] args) {
     // TODO: Implement this assingment
+    String champion = StdIn.readString();
+    Integer total = 1;
+    
+    while (!StdIn.isEmpty()) {
+      String contender = StdIn.readString();
+      
+      if (StdRandom.bernoulli(1 / ++total)) {
+        champion = contender;
+      }
+    }
+
+    System.out.println(champion);
   }
 }
